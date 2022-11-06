fbb4f57c77ea56e93d10210e7043656112e0d7f3
Zar <petarmiloradovic03@gmail.com>
2022-11-06 11:43:32 +0100

    tavolo

--- models/Tavolo.py
+++ models/Tavolo.py
@@ -1,10 +1,2 @@
-from enums.CardProperty import CardProperty
-from models.ColoredCard import ColoredCard
-
-
 class Tavolo(ColoredCard):
     def __init__(self):
-        super().__init__(CardProperty.PLUS_FOUR)
-
-    def __str__(self):
-        return "(t: %s, p: %s)" % (self.type, self.type_property)
